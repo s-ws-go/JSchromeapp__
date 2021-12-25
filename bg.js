45beb06504c9d70fa7f4eec48c3eb565c88fc04b
s-ws-go <aplord@naver.com>
2021-12-25 18:16:31 +0900

    modified with 'git clone'

--- bg.js
+++ bg.js
@@ -1,28 +1,22 @@
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 const IMG_NUMBER = 3;
 
 function paintImage(imgNumber) {
-    const image = new Image();
-    image.src = `images/${imgNumber+1}.jpg`;
-    image.classList.add(`bgImage`);
-    body.prepend(image);
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  image.classList.add(`bgImage`);
+  body.prepend(image);
 } //appendchild 안 쓰고 prepend 쓴 이유는?
 
 function genRandom() {
-    const number = Math.floor(Math.random()*IMG_NUMBER);
-    return number;
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
 }
 
-function init () {
-    const randomnumber = genRandom()
-    paintImage(randomnumber);
-
+function init() {
+  const randomnumber = genRandom();
+  paintImage(randomnumber);
 }
 
 init();
-
-// 자바스크립트엔 math라는 모듈있음
-// 1~5사이의 배경 랜덤추출시
-// Math.random()*5
-// Math.floor(Math.random()*3);
--- weather.js
+++ weather.js
@@ -1,58 +1,54 @@
-const weather = document.querySelector(".js-weather");
+const weather = document.querySelector('.js-weather');
 
-const API_KEY = "cf103dc043464dfe8445c7d6ad4a9cc0";
-const COORDS = "coords";
-
+const API_KEY = 'cf103dc043464dfe8445c7d6ad4a9cc0';
+const COORDS = 'coords';
 
 function getWeather(lat, lng) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
-  )
-    .then(function(response) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    .then(function (response) {
       return response.json();
     })
-    .then(function(json) {
+    .then(function (json) {
       const temperature = json.main.temp;
       const place = json.name;
       weather.innerText = `${temperature} @ ${place}`;
-    });  
+    });
 }
 
-
-function saveCoords (coordsObj) {
+function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
+  console.log(latitude, longitude);
+
   const coordsObj = {
     latitude,
-    longitude
+    longitude,
   };
   saveCoords(coordsObj);
-  getWeather(lat, lng)    // 여기는 세미콜론 왜 안쓰지? 함수 마지막?
+  getWeather(lat, lng);
 }
 
 function handleGeoError() {
   console.log("Can't access geo location");
 }
 
-
-
 function askForCoords() {
   navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
 function loadCoords() {
-    const loadedCoords = localStorage.getItem(COORDS);
-    if(loadedCoords === null) {
-      askForCoords();
-    } else {
-      const parseCoords = JSON.parse(loadedCoords);
-      getWeather(parseCoords.latitude, parseCoords.longitude);
-    }
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
 }
-
 
 function init() {
   loadCoords();
